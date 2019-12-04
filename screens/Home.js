f9f4a141323cccf77bbd6fb6cd497e346e618ca6
Rits1272 <ritikjain1272@gmail.com>
2019-12-04 15:22:03 +0530

    Configured the geolocation api and added marker

--- screens/Home.js
+++ screens/Home.js
@@ -1,43 +1,125 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+// Import BackgroundGeolocation + any optional interfaces
+import BackgroundGeolocation from 'react-native-background-geolocation';
+import React, {Component} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
 import MapView from 'react-native-maps';
+import {Marker} from 'react-native-maps';
 
-export default class Home extends React.Component {
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      region: {
+        latitude: 22.7183992,
+        longitude: 75.8806232,
+        latitudeDelta: 0.001,
+        longitudeDelta: 0.01,
+      },
+    };
+    this.onLocation = this.onLocation.bind(this);
+    this.onMotionChange = this.onMotionChange.bind(this);
+  }
 
-    constructor(props){
-        super(props)
-        this.state={
-            region : {
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-            }
+  static navigationOptions = {
+    title: 'Home',
+  };
+
+  componentDidMount() {
+    // This handler fires whenever bgGeo receives a location update.
+    BackgroundGeolocation.onLocation(this.onLocation, this.onError);
+
+    // This handler fires when movement states changes (stationary->moving; moving->stationary)
+    BackgroundGeolocation.onMotionChange(this.onMotionChange);
+
+    // This event fires when a change in motion activity is detected
+    BackgroundGeolocation.onActivityChange(this.onActivityChange);
+
+    // This event fires when the user toggles location-services authorization
+    BackgroundGeolocation.onProviderChange(this.onProviderChange);
+
+    BackgroundGeolocation.ready(
+      {
+        // Geolocation Config
+        desiredAccuracy: BackgroundGeolocation.DESIRED_ACCURACY_HIGH,
+        distanceFilter: 10,
+        // Activity Recognition
+        stopTimeout: 1,
+        // Application config
+        debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
+        logLevel: BackgroundGeolocation.LOG_LEVEL_VERBOSE,
+        stopOnTerminate: false, // <-- Allow the background-service to continue tracking when user closes the app.
+        startOnBoot: true, // <-- Auto start tracking when device is powered-up.
+        // HTTP / SQLite config
+        url: 'http://yourserver.com/locations',
+        batchSync: false, // <-- [Default: false] Set true to sync locations to server in a single HTTP request.
+        autoSync: true, // <-- [Default: true] Set true to sync each location to server as it arrives.
+        headers: {
+          // <-- Optional HTTP headers
+          'X-FOO': 'bar',
+        },
+        params: {
+          // <-- Optional HTTP params
+          auth_token: 'maybe_your_server_authenticates_via_token_YES?',
+        },
+      },
+      state => {
+        console.log(
+          '- BackgroundGeolocation is configured and ready: ',
+          state.enabled,
+        );
+
+        if (!state.enabled) {
+          BackgroundGeolocation.start(function() {
+            console.log('- Start success');
+          });
         }
-    }
-
-    getInitialState = () => {
-        return {
-          region: {
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          },
-        };
-      }
-
-      onRegionChange(region) {
-        this.setState({ region });
-      }
-      
-      
-  render() {
-    return (
-        <MapView
-        region={this.state.region}
-        onRegionChange={this.onRegionChange}
-      />
+      },
     );
   }
-}
+
+  // You must remove listeners when your component unmounts
+
+  onLocation(location) {
+    var latitude = location['coords'].latitude;
+    var longitude = location['coords'].longitude;
+    this.setState({
+      region: {
+        latitude: latitude,
+        longitude: longitude,
+      },
+    });
+    console.log('[location] -', location);
+  }
+  onError(error) {
+    console.warn('[location] ERROR -', error);
+  }
+  onActivityChange(event) {
+    console.log('[activitychange] -', event); // eg: 'on_foot', 'still', 'in_vehicle'
+  }
+  onProviderChange(provider) {
+    console.log('[providerchange] -', provider.enabled, provider.status);
+  }
+  onMotionChange(event) {
+    console.log('[motionchange] -', event.isMoving, event.location);
+  }
+
+  render() {
+    if(this.onActivityChange){
+      console.log("high")
+    }
+      return (
+        <View style={{flex: 1}}>
+          <MapView style={styles.map} initialRegion={this.state.region}>
+            <Marker coordinate={this.state.region} />
+          </MapView>
+        </View>
+      );
+    }
+  
+  }
+
+const styles = StyleSheet.create({
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
